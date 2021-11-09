39f76914773ef6bc4bf29fb4bac706bf16a5d6a6
Artem <artemkunik@gmail.com>
2021-11-09 21:23:41 +0100

    To make it more clear

--- CCKata/src/com/cckata/Main.java
+++ CCKata/src/com/cckata/Main.java
@@ -53,3 +53,39 @@ public class Main {
 
 
 }
+
+class CardTransaction extends ListNode<Double> {
+    String ARN;
+    String CustomerId;
+    Currency currency;
+    CardTransaction next;
+    String merchant;
+
+    public CardTransaction(Double _value, Currency ccy, String ARN, String customerId, String merchant) {
+        super(_value);
+        this.ARN = ARN;
+        this.CustomerId = customerId;
+        this.currency = ccy;
+        this.merchant = merchant;
+    }
+}
+
+class ListNode<T> {
+    T value;
+    ListNode<T> next;
+
+    public ListNode(T _value) {
+        value = _value;
+        next = null;
+    }
+
+    public ListNode() {
+        value = null;
+        next = null;
+    }
+
+    public ListNode(T _value, ListNode _next) {
+        value = _value;
+        next = _next;
+    }
+}
